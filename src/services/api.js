d005e76f37a60c415d21413e2dac224160bf2717
OlafurKonrad <40210174+OlafurKonrad@users.noreply.github.com>
2025-06-10 19:18:57 +0000

    small fix part 2

--- src/services/api.js
+++ src/services/api.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 
 const isProduction = import.meta.env.PROD; // Vite's way to check for production mode
 
-const baseURL = isProduction
+const API_BASE_URL = isProduction
   ? 'https://olafur-design-backend.onrender.com' // Replace with your actual Render URL, e.g., 'https://olafur-design-backend.onrender.com/api'
   : 'http://localhost:3000/api'; // Your local backend URL (Node.js)
 
-
+  
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/', // Base URL for your backend API
-  headers: {
+  baseURL: API_BASE_URL, // Base URL for your backend API
+  headers: { 
     'Content-Type': 'application/json'
     // Add any other headers like authorization tokens here
   },
